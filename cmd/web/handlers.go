6446699bfd4e3969e842bd4066cade022bf71487
amirintech <amir224030@bue.edu.eg>
2024-06-26 21:27:58 +0300

    forms

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,6 +6,8 @@ import (
 	"github.com/amirintech/snippet-box/internal/models"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 )
 
 func (a *app) homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -43,13 +45,55 @@ func (a *app) snippetViewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (a *app) snippetFormHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Snippet Form"))
+	data := a.newTemplateData(r)
+	a.render(w, r, http.StatusOK, "create.tmpl.html", data)
 }
 
 func (a *app) snippetCreateHandler(w http.ResponseWriter, r *http.Request) {
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := 10
+	// parse & extract form data
+	if err := r.ParseForm(); err != nil {
+		a.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.FormValue("title")
+	content := r.FormValue("content")
+	expires, err := strconv.Atoi(r.FormValue("expires"))
+	if err != nil {
+		a.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// validate form fields
+	fieldErrors := make(map[string]string)
+
+	if strings.TrimSpace(title) == "" {
+		fieldErrors["title"] = "Title cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		fieldErrors["title"] = "Title cannot be more than 100 characters"
+	}
+
+	if strings.TrimSpace(content) == "" {
+		fieldErrors["content"] = "Content cannot be blank"
+	} else if utf8.RuneCountInString(content) > 50_000 {
+		fieldErrors["content"] = "Content cannot be more than 50,000 characters"
+	}
+
+	const (
+		day  = 1
+		week = 7
+		year = 365
+	)
+	if expires != day && expires != week && expires != year {
+		fieldErrors["expires"] = "Expires value must be 1, 7, or 365"
+	}
+
+	if len(fieldErrors) > 0 {
+		fmt.Fprint(w, fieldErrors)
+		return
+	}
+
+	// create snippet
 	snippet, err := a.snippetModel.Insert(title, content, expires)
 	if err != nil {
 		a.serverError(w, r, err)
